fd4c6cbcfae48f0d9060e9aece60c4fe18863409
Venus <venuslu5499@gmail.com>
2020-12-08 14:56:33 +0800

    added disk scheduling file

---
+++ CS207_DiskSched_Algorithms.py
@@ -0,0 +1,26 @@
+import math
+
+completion_time = []
+turnaround_time = []
+waiting_time = []
+processes = []
+
+def FCFS_time(num_processes, arrival_time, burst_time): 
+    pass
+
+def SSTF_time(num_processes, arrival_time, burst_time): 
+    pass
+
+def Scan_time(num_processes, arrival_time, burst_time):  
+    pass
+
+def Look_time(num_processes, arrival_time, burst_time):  
+    pass
+
+def CScan_time(num_processes, arrival_time, burst_time):  
+    pass
+
+def CLook_time(num_processes, arrival_time, burst_time):  
+    pass
+
+

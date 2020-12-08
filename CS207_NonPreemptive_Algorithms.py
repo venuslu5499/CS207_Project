f91a494ddcfc980157cb02b168eecb3e26db0898
Venus <venuslu5499@gmail.com>
2020-12-08 14:46:01 +0800

    added codes

---
+++ CS207_NonPreemptive_Algorithms.py
@@ -0,0 +1,96 @@
+import math
+import numpy as np
+
+completion_time = []
+turnaround_time = []
+waiting_time = []
+processes = []
+
+def FCFS_time(num_processes, arrival_time, burst_time): 
+    burst_time_sorted = []
+    waiting_time_sorted = []
+    turnaround_time_sorted = []
+    for i in range(num_processes):
+        completion_time.append(0)
+        turnaround_time.append(0)
+        waiting_time.append(0)
+        burst_time_sorted.append(0)
+        waiting_time_sorted.append(0)
+        turnaround_time_sorted.append(0)
+    
+    idx_list_sorted = np.argsort(arrival_time)
+    for idx, sorted_idx in enumerate(idx_list_sorted):
+        burst_time_sorted[idx] = burst_time[sorted_idx]
+
+    waiting_time[i] = 0
+    for i in range(1, num_processes):
+        waiting_time[i] = burst_time_sorted[i-1] + waiting_time[i-1]
+    
+    for i, j in enumerate(idx_list_sorted):
+        waiting_time_sorted[j] = waiting_time[i]
+
+    for i in range(num_processes):
+        turnaround_time[i] = burst_time_sorted[i] + waiting_time[i]
+    
+    for i, j in enumerate(idx_list_sorted):
+        turnaround_time_sorted[j] = turnaround_time[i]
+    
+    print(waiting_time_sorted)
+    print(turnaround_time_sorted)
+    print(sum(waiting_time)/len(waiting_time))
+    print(sum(turnaround_time)/len(turnaround_time))
+
+def SJF_time():  
+    pass
+
+def Prio_time(num_processes, arrival_time, burst_time, priority):  
+    for i in range(num_processes):
+        completion_time.append(0)
+        turnaround_time.append(0)
+        waiting_time.append(0)
+
+    for i in range(num_processes):
+        limit = num_processes-i-1
+        for j in range(limit):
+            if arrival_time[j] > arrival_time[j+1]: #check arrival time first
+                temp = arrival_time[j]
+                arrival_time[j] = arrival_time[j+1]
+                arrival_time[j+1] = temp
+                
+                temp = burst_time[j]
+                burst_time[j] = burst_time[j+1]
+                burst_time[j+1] = temp
+
+                temp = priority[j]
+                priority[j] = priority[j+1]
+                priority[j+1] = temp
+
+            if priority[j] > priority[j+1]: #check priority
+                temp = arrival_time[j]
+                arrival_time[j] = arrival_time[j+1]
+                arrival_time[j+1] = temp
+                
+                temp = burst_time[j]
+                burst_time[j] = burst_time[j+1]
+                burst_time[j+1] = temp
+
+                temp = priority[j]
+                priority[j] = priority[j+1]
+                priority[j+1] = temp
+
+    completion_time[0] = arrival_time[0] + burst_time[0]
+    turnaround_time[0] = completion_time[0] - arrival_time[0]
+    waiting_time[0] = turnaround_time[0] - burst_time[0]
+
+    for i in range(1, num_processes):
+        completion_time[i] = burst_time[i] + completion_time[i-1]
+        turnaround_time[i] = completion_time[i] - arrival_time[i]
+        waiting_time[i] = turnaround_time[i] - burst_time[i]
+
+    print(completion_time)
+    print(waiting_time)
+    print(turnaround_time)
+    print(sum(waiting_time)/len(waiting_time))
+    print(sum(turnaround_time)/len(turnaround_time))
+
+
---
+++ CS207_Program.py
@@ -0,0 +1,72 @@
+import math
+import CS207_Preemptive_Algorithms as pre
+import CS207_NonPreemptive_Algorithms as nonpre
+
+def input_arrival_time(num_process):
+    arrival_time = []
+    print("Input Individual Arrival Time")
+    for i in range(1, num_process+1):
+        time = input("AT" +str(i) +": ")
+        arrival_time.append(int(time))
+    return arrival_time
+
+def input_burst_time(num_process):
+    burst_time = []
+    print("Input Individual Burst Time")
+    for i in range(1, num_process+1):
+        time = input("BT" +str(i) +": ")
+        burst_time.append(int(time))
+    return burst_time
+
+def input_priority_number(num_process):
+    priority_num = []
+    print("Input Individual Priority Number")
+    for i in range(1, num_process+1):
+        priority = input("Prio" +str(i) +": ")
+        priority_num.append(int(priority))
+    return priority_num
+
+def input_scheduling_algorithm():
+    print("Input Individual Burst Time")
+    print("CPU Scheduling Algorithm: \n"
+        +"[A] Shortest Remaining Time First (SRTF) \n"
+        +"[B] First Come First Serve (FCFS) \n"
+        +"[C] Priority (Non-Preemptive) \n"
+        +"[D] Shortest Job First (SJF) \n"
+        +"[F] Exit ")
+    algorithm = input("Enter Choice: ")
+    return algorithm
+
+program = True
+while program == True:
+    num_process = int(input("Input no. of Processes [2-9]: "))
+    if num_process < 2 or num_process > 9:
+        print("Must be a number between 2-9")
+    else:
+        arrival_time = input_arrival_time(num_process)
+        burst_time = input_burst_time(num_process)
+        algorithm = input_scheduling_algorithm()
+
+        if algorithm == "A":
+            pre.SRTF_time(num_process, arrival_time, burst_time)
+
+        elif algorithm == "B":
+            nonpre.FCFS_time(num_process, arrival_time, burst_time)
+        
+        elif algorithm == "C":
+            priority = input_priority_number(num_process)
+            nonpre.Prio_time(num_process, arrival_time, burst_time, priority)
+        
+        elif algorithm == "D":
+            nonpre.SJF_time(num_process, arrival_time, burst_time)
+
+        elif algorithm == "F":
+            again = input("Input Again (y/n)? ")
+            if again == "y":
+                continue
+            elif again == "n":
+                break
+    
+
+
+

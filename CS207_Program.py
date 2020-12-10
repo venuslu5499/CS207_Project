c9bf3af50663f8e1c3dbf425cbe98481e65d1461
stephchen817 <66129328+stephchen817@users.noreply.github.com>
2020-12-10 17:04:20 +0800

    edited inputs

--- CS207_Program.py
+++ CS207_Program.py
@@ -3,63 +3,66 @@ import CS207_Preemptive_Algorithms as pre
 import CS207_NonPreemptive_Algorithms as nonpre
 import CS207_DiskSched_Algorithms as disk
 
+
 def input_arrival_time(num_process):
     arrival_time = []
     print("Input Individual Arrival Time")
     for i in range(1, num_process+1):
-        time = input("AT" +str(i) +": ")
+        time = input("AT" + str(i) + ": ")
         arrival_time.append(int(time))
     return arrival_time
+
 
 def input_burst_time(num_process):
     burst_time = []
     print("Input Individual Burst Time")
     for i in range(1, num_process+1):
-        time = input("BT" +str(i) +": ")
+        time = input("BT" + str(i) + ": ")
         burst_time.append(int(time))
     return burst_time
+
 
 def input_priority_number(num_process):
     priority_num = []
     print("Input Individual Priority Number")
     for i in range(1, num_process+1):
-        priority = input("Prio" +str(i) +": ")
+        priority = input("Prio" + str(i) + ": ")
         priority_num.append(int(priority))
     return priority_num
+
 
 def input_scheduling_algorithm():
     print("Input Individual Burst Time")
     print("CPU Scheduling Algorithm: \n"
-        +"[A] Shortest Remaining Time First (SRTF) \n"
-        +"[B] First Come First Serve (FCFS) \n"
-        +"[C] Priority (Non-Preemptive) \n"
-        +"[D] Shortest Job First (SJF) \n"
-        +"[E] Exit ")
+          + "[A] Shortest Remaining Time First (SRTF) \n"
+          + "[B] First Come First Serve (FCFS) \n"
+          + "[C] Priority (Non-Preemptive) \n"
+          + "[D] Shortest Job First (SJF) \n"
+          + "[E] Exit ")
     algorithm = input("Enter Choice: ")
     return algorithm
 
+
 def input_request_sequence(num_request):
     request_sequence = []
-    for i in range(1,num_request+1):
+    for i in range(1, num_request+1):
         location = input("Loc1"+str(i)+": ")
         request_sequence.append(int(location))
     return request_sequence
 
+
 def input_disk_scheduling_algorithm():
-    print("Input current position:")
-    print("Input track size: ")
-    print("Input seek rate: ")
-    #print("Input number of request [max. of 10]: ")
     print("Disk Scheduling Algorithm: \n"
-        +"[A] First Come First Serve (FCFS) \n"
-        +"[B] Shortest Seek Time First (SSTF) \n"
-        +"[C] Scan \n"
-        +"[D] Look \n"
-        +"[E] Circular Scan (CSCAN) \n"
-        +"[F] Circular Look (CLOOK) \n"
-        +"[G] Exit ")
+          + "[A] First Come First Serve (FCFS) \n"
+          + "[B] Shortest Seek Time First (SSTF) \n"
+          + "[C] Scan \n"
+          + "[D] Look \n"
+          + "[E] Circular Scan (CSCAN) \n"
+          + "[F] Circular Look (CLOOK) \n"
+          + "[G] Exit ")
     disk_sched_algorithm = input("Enter Choice: ")
     return disk_sched_algorithm
+
 
 program = True
 while program == True:
@@ -76,11 +79,11 @@ while program == True:
 
         elif algorithm == "B":
             nonpre.FCFS_time(num_process, arrival_time, burst_time)
-        
+
         elif algorithm == "C":
             priority = input_priority_number(num_process)
             nonpre.Prio_time(num_process, arrival_time, burst_time, priority)
-        
+
         elif algorithm == "D":
             nonpre.SJF_time(num_process, arrival_time, burst_time)
 
@@ -90,13 +93,13 @@ while program == True:
                 continue
             elif again == "n":
                 break
-    
-# suggested flow 
+
+# suggested flow
 program = True
 while program == True:
     scheduling = input("Choose scheduling type: \n"
-                        +"[1] CPU Scheduling \n"
-                        +"[2] Disk Scheduling \n")
+                       + "[1] CPU Scheduling \n"
+                        + "[2] Disk Scheduling \n")
     if scheduling == "1":
         num_process = int(input("Input no. of Processes [2-9]: "))
         if num_process < 2 or num_process > 9:
@@ -111,11 +114,12 @@ while program == True:
 
             elif algorithm == "B":
                 nonpre.FCFS_time(num_process, arrival_time, burst_time)
-            
+
             elif algorithm == "C":
                 priority = input_priority_number(num_process)
-                nonpre.Prio_time(num_process, arrival_time, burst_time, priority)
-            
+                nonpre.Prio_time(num_process, arrival_time,
+                                 burst_time, priority)
+
             elif algorithm == "D":
                 nonpre.SJF_time(num_process, arrival_time, burst_time)
 
@@ -126,32 +130,37 @@ while program == True:
                 elif again == "n":
                     break
     elif scheduling == "2":
+        current_position = int(input("Input current position:"))
+        track_size = int(input("Input track size: "))
+        seek_rate = int(input("Input seek rate: "))
         num_request = int(input("Input number of request [max of 10]: "))
-        # di ko alam ano minimum huhubels
+       
         if num_request > 10:
             print("The program will only be accepting a maximum of 10 requests.")
         else:
             request_sequence = input_request_sequence(num_request)
             disk_algo = input_disk_scheduling_algorithm()
-
             if disk_algo == "A":
-                disk.FCFS_time(current_position, track_size, seek_rate, request_sequence)
+                disk.FCFS_time(current_position, track_size,
+                               seek_rate, request_sequence)
             elif disk_algo == "B":
-                disk.SSTF_time(current_position, track_size, seek_rate, request_sequence)
+                disk.SSTF_time(current_position, track_size,
+                               seek_rate, request_sequence)
             elif disk_algo == "C":
-                disk.Scan_time(current_position, track_size, seek_rate, request_sequence)
+                disk.Scan_time(current_position, track_size,
+                               seek_rate, request_sequence)
             elif disk_algo == "D":
-                disk.Look_time(current_position, track_size, seek_rate, request_sequence)
+                disk.Look_time(current_position, track_size,
+                               seek_rate, request_sequence)
             elif disk_algo == "E":
-                disk.CScan_time(current_position, track_size, seek_rate, request_sequence)
+                disk.CScan_time(current_position, track_size,
+                                seek_rate, request_sequence)
             elif disk_algo == "F":
-                disk.CLook_time(current_position, track_size, seek_rate, request_sequence)
+                disk.CLook_time(current_position, track_size,
+                                seek_rate, request_sequence)
             elif disk_algo == "G":
                 again = input("Input Again (y/n)? ")
                 if again == "y":
                     continue
                 elif again == "n":
                     break
-            
-
-

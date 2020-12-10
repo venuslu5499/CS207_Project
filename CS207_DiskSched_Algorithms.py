6040394f40ed34a7650ed4d4333c0ad0ed40fe04
stephchen817 <66129328+stephchen817@users.noreply.github.com>
2020-12-10 23:36:22 +0800

    added sstf but not sure if it's working yet kekekeke

--- CS207_DiskSched_Algorithms.py
+++ CS207_DiskSched_Algorithms.py
@@ -37,8 +37,32 @@ def FCFS_time(num_requests, current_position, request_locations):
     print("Seek Time: ", (seek_count/num_requests))
 
 
-def SSTF_time(current_position, track_size, seek_rate, request_sequencee): 
-   pass
+def SSTF_time(current_position, track_size, request_sequence): 
+    if(len(request_sequence)==0):
+        return
+    num_request = len(request_sequence)
+    difference = [0]*1
+    
+    for i in range(num_request): 
+        difference[i] = [0, 0]
+    
+    head_movement = 0
+    seek_sequence = [0]*(num_request+1)
+    
+    for i in range(num_request):
+        seek_sequence[i] = current_position
+        get_difference(request_sequence, current_position, difference)
+        index = find_shortest_distance(difference)
+        difference[index][1] = True
+        head_movement += difference[index][0]
+
+    seek_sequence[len(seek_sequence)-1] = current_position
+    print("Total head movement: ", head_movement)   
+    print("Seek sequence is: ",seek_sequence)
+    
+    for i in range(num_request+1):
+        print(seek_sequence[i])
+   
 
 def Scan_time(current_position, track_size, seek_rate, request_sequence):  
     pass
